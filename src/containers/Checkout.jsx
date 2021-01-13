d370b71e31c9accf8fcd3772d37e100b7e118717
Carlos Lopez <krlosaren@gmail.com>
2021-01-12 22:08:46 -0300

    success paymenet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -22,38 +22,51 @@ const Checkout = () => {
   };
 
   return (
-    <div className="Checkout">
-      <div className="Checkout-content">
+    <div className='Checkout'>
+      <div className='Checkout-content'>
         {cart.length > 0 ? (
           <h3> Lista de Pedidos: </h3>
         ) : (
-          <h3>Sin pedidos...</h3>
-        )}
+            <h3>Sin pedidos...</h3>
+          )}
         {cart.map((item) => (
-          <div className="Checkout-item" key={item.id}>
-            <div className="Checkout-element">
-              <h4>{item.title} Name</h4>
-              <span>${item.price}</span>
+          <div className='Checkout-item' key={item.id}>
+            <div className='Checkout-element'>
+              <h4>
+                {item.title}
+                {' '}
+                Name
+              </h4>
+              <span>
+                $
+                {item.price}
+              </span>
             </div>
-            <button type="button" onClick={handleRemove(item)}>
-              <i className="fas fa-trash-alt" />
+            <button type='button' onClick={handleRemove(item)}>
+              <i className='fas fa-trash-alt' />
             </button>
           </div>
         ))}
         {cart.length > 0 && (
-          <div className="Checkout-element">
-            <h3 className="total-down">Precio Total</h3>
+          <div className='Checkout-element'>
+            <h3 className='total-down'>Precio Total</h3>
             <span>
-              <h3> {`$ ${handleSumTotal()}`}</h3>
+              <h3>
+                {' '}
+                {`$ ${handleSumTotal()}`}
+              </h3>
             </span>
           </div>
         )}
       </div>
       {cart.length > 0 && (
-        <div className="Checkout-sidebar">
-          <h3>{`Precio Total:  $ ${handleSumTotal()}`}</h3>
-          <Link to="/checkout/information">
-            <button type="button">Continuar Pedido</button>
+        <div className='Checkout-sidebar'>
+          <h3>
+            {' '}
+            {`Precio Total:  $ ${handleSumTotal()}`}
+          </h3>
+          <Link to='/checkout/information'>
+            <button type='button'>Continuar Pedido</button>
           </Link>
         </div>
       )}
